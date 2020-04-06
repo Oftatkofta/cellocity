f9a030ecc9808016dc3a7068f06ec0a31ce3185b
Jens Eriksson <jenseri@gmail.com>
2020-04-07 00:04:08 +0200

    autiapi test

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../cellocity'))
-autodoc_mock_imports = ['cv2', 'numpy', 'pandas']
+#autodoc_mock_imports = ['cv2', 'numpy', 'pandas']
 
 
 # -- Project information -----------------------------------------------------
@@ -32,8 +32,10 @@ release = '0.0.1alpha'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+extensions = ['sphinx.ext.autodoc', 'autoapi.extension'
 ]
+
+autoapi_dirs = ['../cellocity']
 
 # Both the class' docstring and the docstring in self.__init__() is inserted.
 autoclass_content = 'both'
