d6e0cc8691fb200bc573a46a69763902b6ee99ed
Jens Eriksson <jenseri@gmail.com>
2020-04-08 12:40:54 +0200

    rdt ftw

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../../cellocity/'))
 project = 'Cellocity'
 copyright = '2020, Jens Eriksson'
 author = 'Jens Eriksson'
-master_doc = 'index'
+master_doc = '/source/index'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1alpha'
@@ -59,4 +59,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['/source/_static']
