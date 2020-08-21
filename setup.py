b534de8db630a30834f0fe6d1141599cc375c841
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2020-08-21 14:28:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,17 @@ with open('readme.md') as f:
 
 setup(
     name='cellocity',
-    version='0.0.2',
+    version='0.1.0',
     description='Velocity and vector analysis of microscopy data',
     long_description=readme,
     long_description_content_type='text/markdown',
-    install_requires=["docutils>=0.16"],
+    install_requires=[
+        "docutils>=0.16",
+        "tifffile==2020.2.16",
+        "opencv-python>4.2.0",
+        "numpy>=1.18.1",
+        "pandas>=1.0.1",
+        ],
     author='Jens Eriksson',
     author_email='jens.eriksson@imbim.uu.se',
     url='https://github.com/oftatkofta/cellocity',
@@ -21,8 +27,9 @@ setup(
     packages=find_packages(exclude=('tests', 'docs', '.idea')),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Topic :: Scientific/Engineering",
     ],
     python_requires='>=3.6',
