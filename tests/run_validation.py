b12f12140817ed8b786e87ff69409d2341b8f805
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2020-08-28 11:13:36 +0200

    Update run_validation.py

--- tests/run_validation.py
+++ tests/run_validation.py
@@ -1,8 +1,29 @@
+import sys, getopt
 from cellocity import validation
 from pathlib import Path
 
-inpath = Path(r"C:\Users\Jens\Documents\_Microscopy\FrankenScope2\Calibration stuff\DIC_truth")
-outpath3 = Path(r"C:\Users\Jens\Desktop\temp3")
 
 if __name__ == "__main__":
     validation.run_validation(inpath, outpath3)
+def main(argv):
+
+    inputfolder = ''
+    outputfolder = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["infolder=","outfolder="])
+    except getopt.GetoptError:
+        print ('validation.py -i <infolder> -o <outfolder>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('validation.py -i <infolder> -o <outfolder>')
+            sys.exit()
+        elif opt in ("-i", "--infolder"):
+            inpath = Path(arg)
+        elif opt in ("-o", "--outfolder"):
+            outpath = Path(arg)
+
+    run_validation(inpath, outpath)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
