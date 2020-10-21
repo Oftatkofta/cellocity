e55f9e2a1cb02d686e7cf278564a256d993e176b
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2020-10-21 14:47:51 +0200

    Update five_sigma_tester.py

--- tests/five_sigma_tester.py
+++ tests/five_sigma_tester.py
@@ -8,16 +8,30 @@ import pandas as pd
 import seaborn as sns
 
 
-def combine_lcorr_and_process_time_to_df(lcorrdf, processtimedict, file_name, method):
+def combine_lcorr_and_process_time_to_df(lcorrdf, processtimedict, file_name, analyzer_name):
 
-    df1 = pd.DataFrame.from_dict(processtimedict, orient='index', columns=["process_time_s"])
+    df1 = pd.DataFrame.from_dict(processtimedict, orient='index', columns=["process_time"])
     df1.sort_index(inplace=True)
-    lcorrdf["process_time_s"] = df1["process_time_s"]
+    lcorrdf["process_time"] = df1["process_time"]
     lcorrdf["file_name"] = file_name
-    lcorrdf["method"] = method
+    lcorrdf["analyzer"] = analyzer_name
 
+    fields = file_name.split("_")
+    magnification = fields[3]
+    displacemet = fields[4] + " " + fields[5]
+    df["magnification"] = magnification
+    df["displacement"] = displacemet
+
+    if "MED" in file_name:
+        filter_name = "Median"
+    else:
+        filter_name = "None"
+
+    lcorrdf["filter"] = filter_name
 
     return lcorrdf
+
+
 
 def main():
 
@@ -28,20 +42,30 @@ def main():
     saveme = outpath / "5sigma.csv"
     for ch in ch_list:
         print(ch.name)
+
         a1 = make_fb_flow_analyzer(ch)
         fsig = FiveSigmaAnalysis(a1)
         fsig.calculateCorrelationAllFrames()
         lcorrdf = fsig.getCorrelationLengthsAsDf()
         processtimedict = fsig._process_times
-        alldata = alldata.append(combine_lcorr_and_process_time_to_df(lcorrdf, processtimedict, ch.name, "opt_flow"))
+        alldata = alldata.append(combine_lcorr_and_process_time_to_df(lcorrdf,
+                                                                      processtimedict,
+                                                                      ch.name,
+                                                                      "optical_flow"
+                                                                      ))
         a2 = make_piv_analyzer(ch)
         fsig = FiveSigmaAnalysis(a2)
         fsig.calculateCorrelationAllFrames()
         lcorrdf = fsig.getCorrelationLengthsAsDf()
         processtimedict = fsig._process_times
-        alldata = alldata.append(combine_lcorr_and_process_time_to_df(lcorrdf, processtimedict, ch.name, "PIV"))
-        print(alldata)
+        alldata = alldata.append(combine_lcorr_and_process_time_to_df(lcorrdf,
+                                                                      processtimedict,
+                                                                      ch.name,
+                                                                      "PIV"
+                                                                      ))
+
 
     alldata.to_csv(saveme)
+
 if __name__ == "__main__":
     main()
