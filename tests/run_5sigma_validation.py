f51eb6417bad035b9cd5f19a4c8b6f51420a7cd6
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2020-10-23 12:05:25 +0200

    Update run_5sigma_validation.py

--- tests/run_5sigma_validation.py
+++ tests/run_5sigma_validation.py
@@ -3,8 +3,7 @@ from cellocity import validation
 from pathlib import Path
 
 """
-This script runs the validation suite for FiveSigmaAnalysis. It takes a long time to run fully, a short option where
-only one frame per file is available by running  
+This script runs the validation suite for FiveSigmaAnalysis. It takes a long time to run fully.
 """
 
 
@@ -12,18 +11,18 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, "hi:o:", ["infolder=", "outfolder="])
     except getopt.GetoptError:
-        print('validation.py -i <infolder> -o <outfolder>')
+        print('python run_5sigma_validation.py -i <infolder> -o <outfolder>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('validation.py -i <infolder> -o <outfolder>')
+            print('python run_5sigma_validation -i <infolder> -o <outfolder>')
             sys.exit()
         elif opt in ("-i", "--infolder"):
             inpath = Path(arg)
         elif opt in ("-o", "--outfolder"):
             outpath = Path(arg)
 
-    validation.run_base_validation(inpath, outpath)
+    validation.run_5sigma_validation(inpath, outpath)
 
 
 if __name__ == "__main__":
