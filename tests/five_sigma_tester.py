1429de52ddd2caa3db89cfd4e04874cc05d93888
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2020-10-23 11:49:40 +0200

    Update five_sigma_tester.py

--- tests/five_sigma_tester.py
+++ tests/five_sigma_tester.py
@@ -9,6 +9,10 @@ import seaborn as sns
 
 
 def combine_lcorr_and_process_time_to_df(lcorrdf, processtimedict, file_name, analyzer_name):
+    """
+    Performs combination and mutation of correlation length dataframe to simplify visualization and plotting
+    Used by 5sigma_validation.
+    """
 
     df1 = pd.DataFrame.from_dict(processtimedict, orient='index', columns=["process_time"])
     df1.sort_index(inplace=True)
@@ -19,8 +23,8 @@ def combine_lcorr_and_process_time_to_df(lcorrdf, processtimedict, file_name, an
     fields = file_name.split("_")
     magnification = fields[3]
     displacemet = fields[4] + " " + fields[5]
-    df["magnification"] = magnification
-    df["displacement"] = displacemet
+    lcorrdf["magnification"] = magnification
+    lcorrdf["displacement"] = displacemet
 
     if "MED" in file_name:
         filter_name = "Median"
@@ -29,21 +33,57 @@ def combine_lcorr_and_process_time_to_df(lcorrdf, processtimedict, file_name, an
 
     lcorrdf["filter"] = filter_name
 
+    #max_lcorrs are given by pixel size * pixels per image column -1 (1607 for the test data)
+    max_lcorrs = {
+        "10X": 1805.46,
+        "15X": 1203.64,
+        "40X": 459.60,
+        "60Xopt": 306.94,
+        "60X": 200.88}
+
+    lcorrdf = lcorrdf.assign(
+        max_lcorr=lambda dataframe: dataframe['magnification'].map(lambda max: max_lcorrs.get(max)))
+    lcorrdf = lcorrdf.assign(
+        fraction_of_max_lcorr=lambda df: df['Cvv_um'] / df['max_lcorr'])
+
     return lcorrdf
 
+def make_lcorr_plot(lcorrdf):
+    """
+    Generates a plot comparing correlation lengths between analyzers and magnifications
+
+    """
+    sns_plot = sns.catplot(x="magnification", y="fraction_of_max_lcorr",
+                           hue="filter", col = "analyzer",
+                           data=lcorrdf, kind="box",
+                           height=8, aspect=.7, )
+    sns_plot.set_axis_labels("Magnification", "Fraction of theoretical maximum correlation length")
 
 
-def main():
+    return sns_plot
 
-    inpath = Path(r"C:\Users\Jens\Desktop\S-BSST461")
-    outpath = Path(r"C:\Users\Jens\Desktop\temp")
+def make_proces_time_plot(lcorrdf):
+    """
+    Generates a bar plot comparing processing times for the two analyzers.
+
+    """
+    sns_plot = sns.catplot(x="analyzer", y="process_time",
+                    data=lcorrdf, kind="box",
+                    height=8, aspect=.7)
+
+    sns_plot.set_axis_labels("Analyzer", "Process time (s)")
+
+    return sns_plot
+
+def run_5sigma_validation(inpath, outpath):
+    print("Creating Channel objects...")
     ch_list = make_channels(inpath)
     alldata = pd.DataFrame()
-    saveme = outpath / "5sigma.csv"
+    saveme = outpath / "5sigma_analysis.csv"
     for ch in ch_list:
-        print(ch.name)
-
+        print("Farenback flow analyzer starting on: ", ch.name)
         a1 = make_fb_flow_analyzer(ch)
+        print("5-sigma analysis starting on: ", ch.name)
         fsig = FiveSigmaAnalysis(a1)
         fsig.calculateCorrelationAllFrames()
         lcorrdf = fsig.getCorrelationLengthsAsDf()
@@ -53,7 +93,9 @@ def main():
                                                                       ch.name,
                                                                       "optical_flow"
                                                                       ))
+        print("PIV analyzer starting on: ", ch.name)
         a2 = make_piv_analyzer(ch)
+        print("5-sigma analysis starting on: ", ch.name)
         fsig = FiveSigmaAnalysis(a2)
         fsig.calculateCorrelationAllFrames()
         lcorrdf = fsig.getCorrelationLengthsAsDf()
@@ -64,8 +106,8 @@ def main():
                                                                       "PIV"
                                                                       ))
 
-
     alldata.to_csv(saveme)
 
-if __name__ == "__main__":
-    main()
+    return alldata
+
+
