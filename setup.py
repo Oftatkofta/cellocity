29f65f3bdd88b70153ced387cbb2a6321e05d266
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2020-10-23 13:40:54 +0200

    version bump and cleanup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('readme.md') as f:
 
 setup(
     name='cellocity',
-    version='0.1.2',
+    version='0.1.6',
     description='Velocity and vector analysis of microscopy data',
     long_description=readme,
     long_description_content_type='text/markdown',
