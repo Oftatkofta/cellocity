0a9fcf3067fa4709dd60d35ecbd9047107ef2820
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2020-10-28 10:50:59 +0100

    Version bump to 1.0.0 for submission

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('readme.md') as f:
 
 setup(
     name='cellocity',
-    version='0.1.8',
+    version='1.0.0',
     description='Velocity and vector analysis of microscopy data',
     long_description=readme,
     long_description_content_type='text/markdown',
